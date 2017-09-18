74f5ff9ec6f8b5c1080eaacf32445f3a75e3c2e4
呂敏煌 <luhuangjack@gmail.com>
2017-09-18 17:27:11 +0800

    FindGCD

--- FindGCD/Program.cs
+++ FindGCD/Program.cs
@@ -10,6 +10,11 @@ namespace FindGCD
     {
         static void Main(string[] args)
         {
+            ////GCD 最大公倍數
+            ////lcm 最小公因數
+            //int lcm = GCD(12, 6) * (12 / GCD(12, 6)) * (6 / GCD(12, 6));
+            //Console.WriteLine(GCD(GCD(15, 19),27));
+            //Console.WriteLine(lcm);
         }
     }
 }
