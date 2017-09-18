552a3ebe1585ca60cf329b84e4e863080dbe42ae
呂敏煌 <luhuangjack@gmail.com>
2017-09-18 17:28:51 +0800

    加入函式

--- FindGCD/Program.cs
+++ FindGCD/Program.cs
@@ -12,9 +12,17 @@ namespace FindGCD
         {
             ////GCD 最大公倍數
             ////lcm 最小公因數
-            //int lcm = GCD(12, 6) * (12 / GCD(12, 6)) * (6 / GCD(12, 6));
-            //Console.WriteLine(GCD(GCD(15, 19),27));
-            //Console.WriteLine(lcm);
+            int lcm = GCD(12, 6) * (12 / GCD(12, 6)) * (6 / GCD(12, 6));
+            Console.WriteLine(GCD(GCD(15, 19),27));
+            Console.WriteLine(lcm);
+        }
+        //求最大公因數
+        public static int GCD(int num1, int num2)
+        {
+            if (num1 % num2 == 0)
+                return num2;
+            else
+                return GCD(num2, num1 % num2);
         }
     }
 }
